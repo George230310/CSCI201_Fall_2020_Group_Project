868eefc168fd2affc5496e503afbae094bfd5c76
Raymond Cheung <rlcheung@usc.edu>
2020-11-15 21:05:51 -0500

    Add openjfx maven dependency/plugin to pom.xml
    Add main Connect4GUI, controller and GUI runner

---
+++ src/main/java/edu/usc/csci201/connect4/gui/GameController.java
@@ -0,0 +1,14 @@
+package edu.usc.csci201.connect4.gui;
+ 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+ 
+public class GameController {
+    @FXML private Text actiontarget;
+    
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        actiontarget.setText("Sign in button pressed");
+    }
+
+}
